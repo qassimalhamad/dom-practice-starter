9a39b0862d0884264a175e907451a55beb7ff541
Qasim <qalhamad33@gmail.com>
2024-07-03 10:33:27 +0300

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // empty file, no JS needed.
+
+const sun = document.querySelector('.fa-sun')
+const moon = document.querySelector('.fa-moon')
+const body = document.querySelector('body')
+
+moon.addEventListener('click', () =>{
+    body.className = 'classChangeColor';
+}) 
+
+sun.addEventListener('click' ,()=>{
+    body.classList.remove('classChangeColor')
+} )
